549b8215c9b1839a6a9090ee9924b96edacb90e2
liuming <1401389531@qq.com>
2019-04-21 14:17:15 +0800

    4-21 require

---
+++ requireJs/web/index.js
@@ -0,0 +1,18 @@
+require.config({
+    paths:{
+        'jquery':'../lib/jquery.min',
+        'show':'./show'
+    }
+})
+//如果依赖的模块不在当前目录下 ，需要require.config 进行路径配置 进行映射
+define(['jquery','show'],function($,show){
+    'use strict';
+    $(function(){
+        $('#box').html(`<h1>hell require</h1>`);
+        $('#box').click(function(){
+            show.alertLL();
+        })
+        show.do();
+    })
+
+})
---
+++ requireJs/web/show.js
@@ -0,0 +1,15 @@
+
+define(['jquery'],function($){
+    'use strict';
+    console.log('我是show模块'); 
+    return {
+        do:$(function(){
+            $(document.body).append(`<div>我是动态生成的</div>`)
+        }),
+        alertLL:function(){
+            alert('你点击了我哦')
+        }
+    }
+    
+
+})
